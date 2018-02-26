e9cb34dc3008fe50c3b92622b3c105e2e150351f
Vlad Minaev <minaev@haulmont.com>
2018-02-27 02:09:23 +0400

    Entities filtration

--- src/main/EntitiesList.tsx
+++ src/main/EntitiesList.tsx
@@ -6,26 +6,68 @@ interface Props {
   onEntitySelect: (entity: MetaClassInfo) => void;
 }
 
-export default class EntitiesList extends React.Component<Props> {
+interface State {
+  showSystemEntities: boolean;
+  filterVal?: string;
+}
 
-  selectEntity = (e: React.MouseEvent<HTMLAnchorElement>, metaClass: MetaClassInfo) => {
+export default class EntitiesList extends React.Component<Props, State> {
+
+  state: State = {
+    showSystemEntities: false
+  };
+
+  render() {
+    const {showSystemEntities, filterVal} = this.state;
+    const {metaClasses} = this.props;
+
+    return (
+      <div>
+        <input type="text" placeholder="Filter" value={filterVal} onChange={this.changeFilterVal}/>
+        <label>
+          <input type="checkbox" checked={showSystemEntities} onChange={this.toggleShowSystemEntities}/>
+          Show system entities
+        </label>
+        <ul>
+          {filterMetaClasses(metaClasses, {showSystemEntities, filterVal}).map(metaClass =>
+            <li key={metaClass.entityName}>
+              <a href="#" onClick={(e) => this.selectEntity(e, metaClass)}>
+                {metaClass.entityName}
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+
+  private selectEntity = (e: React.MouseEvent<HTMLAnchorElement>, metaClass: MetaClassInfo) => {
     e.preventDefault();
     this.props.onEntitySelect(metaClass);
   };
 
-  render() {
-    const {metaClasses} = this.props;
-    return (
-      <ul>
-        {metaClasses.map(metaClass =>
-          <li key={metaClass.entityName}>
-            <a href="#" onClick={(e) => this.selectEntity(e, metaClass)}>
-              {metaClass.entityName}
-            </a>
-          </li>
-        )}
-      </ul>
-    );
-  }
+  private toggleShowSystemEntities = (e: React.ChangeEvent<HTMLInputElement>):void => {
+    this.setState({showSystemEntities: e.target.checked});
+  };
 
+  private changeFilterVal = (e: React.ChangeEvent<HTMLInputElement>):void => {
+    this.setState({filterVal: e.target.value});
+  }
+}
+
+function filterMetaClasses(metaClasses: MetaClassInfo[],
+                           {showSystemEntities = false, filterVal}: Partial<State>): MetaClassInfo[] {
+  return metaClasses.filter(metaClass => {
+    if (!showSystemEntities && isSystemClass(metaClass.entityName)) {
+      return false;
+    }
+    if (filterVal && metaClass.entityName.toLowerCase().indexOf(filterVal.toLowerCase()) < 0) {
+      return false;
+    }
+    return true;
+  });
+}
+
+function isSystemClass(className: string): boolean {
+  return className.startsWith('sys$') || className.startsWith('sec$');
 }
